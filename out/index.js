0e392767f6565d3a963e5955937242f262a1d200
olimjon <Devolimjon@gmail.com>
2022-03-29 18:49:45 +0500

    backend

--- out/index.js
+++ out/index.js
@@ -1,7 +1,26 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
+app.get('/paper', (req, res) => {
     res.sendFile('index.html', { root: './pages' });
+});
+app.get('/work', (req, res) => {
+    res.sendFile('index.html', { root: './pages' });
+});
+app.get('/about', (req, res) => {
+    res.sendFile('index1.html', { root: './pages' });
+});
+app.get('/blog', (req, res) => {
+    res.sendFile('index2.html', { root: './pages' });
+});
+app.get('/contact', (req, res) => {
+    res.sendFile('index3.html', { root: './pages' });
 });
 app.listen(8080, () => {
     console.log('server ishga tushdi, 8080 portda');
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import express from 'express'
+import path from 'path'
+
 const app = express()
+app.use(express.static(path.join(__dirname, '../public')))
 
-
-app.get('/', (req, res) =>{
+app.get('/paper', (req, res) =>{
     res.sendFile('index.html', {root: './pages'})
 })
 
 
+app.get('/work', (req, res) =>{
+    res.sendFile('index.html', {root: './pages'})
+})
+
+app.get('/about', (req, res) =>{
+    res.sendFile('index1.html', {root: './pages'})
+})
 
 
+app.get('/blog', (req, res) =>{
+    res.sendFile('index2.html', {root: './pages'})
+})
 
-
+app.get('/contact', (req, res) =>{
+    res.sendFile('index3.html', {root: './pages'})
+})
 
 
 
